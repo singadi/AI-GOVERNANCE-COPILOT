518b6c51e62371edd388a60510d5b9c6815d3263
Aditya Singh <singadi028@gmail.com>
2026-02-14 18:36:50 -0600

    Fix formatting in gold_orders_fact.sql

--- dbt/gov_copilot_dbt/models/gold/gold_orders_fact.sql
+++ dbt/gov_copilot_dbt/models/gold/gold_orders_fact.sql
@@ -12,7 +12,7 @@ select
   cast(o.order_id as integer) as order_id,
   cast(o.customer_id as integer) as customer_id,
   cast(o.amount as numeric(12,2)) as amount,
-  cast(o.order_ts as date) as order_date
+  cast(o.order_ts as date) as order_date,
   lower(trim(c.email)) as email
 from {{ ref('orders') }} o
 join {{ ref('customers') }} c on o.customer_id = c.customer_id 
